787b0aa77f49aaf905a26d43be18dfb7160579a4
Timothy OBrien <obrien.timothy.a@gmail.com>
2025-08-24 11:48:41 +1000

    terniary operators

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -40,13 +40,15 @@ struct ContentView: View {
             
             
             Button("Press Me!") {
-                if(message == message1) {
-                    message = message2
-                    imageString = imageString2
-                } else {
-                    message = message1
-                    imageString = imageString1
-                }
+//                if(message == message1) {
+//                    message = message2
+//                    imageString = imageString2
+//                } else {
+//                    message = message1
+//                    imageString = imageString1
+//                }
+                message = message == message1 ? message2 : message1
+                imageString = imageString == imageString1 ? imageString2 : imageString1
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
